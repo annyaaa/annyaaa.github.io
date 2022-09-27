ec78b8528b5aff9598a8f353774dcde687315f05
Annya Kindout <annyakindout@outlook.fr>
2022-09-27 16:58:06 +0200

    git commit -m "<commit message>"  Veuillez saisir le message de validation pour vos modifications. Les lignes  commençant par '#' seront ignorées, et un message vide abandonne la validation.
     Sur la branche main
     Votre branche est à jour avec 'origin/main'.
    
     Modifications qui seront validées :
    	nouveau fichier : Master/tab.html
    	modifié :         index.html
    	nouveau fichier : pythonA.html
    	nouveau fichier : pythonA.py
    	nouveau fichier : tab.html
    git commit -m "<commit message>"
    Validation

---
+++ pythonA.py
@@ -0,0 +1,6 @@
+def affichefois(ch:str,nombre:int)->None:
+...     """Affiche nombre fois la chaine ch"""
+...     for i in range (0,nombre):
+...             print (ch)
+...
+>>> affichefois("Bonjour!",48)
